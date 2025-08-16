1bd5bb3fc4cdbbf2792a617ac5dd5b16367ef07c
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-16 08:13:15 +0530

    minor fix.

--- src/main/java/com/ahmad/learning/spring_security/jwt/JwtUtills.java
+++ src/main/java/com/ahmad/learning/spring_security/jwt/JwtUtills.java
@@ -1,10 +1,16 @@
 package com.ahmad.learning.spring_security.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Date;
+
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 public class JwtUtills{
@@ -22,14 +28,42 @@ public class JwtUtills{
 
     }
 
+
+
     //method for extracting claims
+    public Claims extractAllClaim(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
 
+    }
 
-    //method for validating token.
-    //method for generating token
 
     //method for extracting username
-    //method for creating signkey
+    public String extractUsername(String token){
+       return  extractAllClaim(token).getSubject();
+    }
+
+    //method for check expiration of token.
+    private Boolean isTokenExpired(String token){
+        return extractAllClaim(token).getExpiration().before(new Date());
+    }
+
+    //method for generating token
+
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+10800000))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    //method for validating jwt token
 
 
 
