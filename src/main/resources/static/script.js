d6354ef3133114e14759a01d7d7d20496b6123e6
Ahmad Raza <mustafaraza03898@gmail.com>
2025-08-31 14:48:20 +0530

    completed frontend work

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,15 +1,24 @@
+const searchBox = document.querySelector('.field'); 
+const suggestionBox = document.querySelector('.suggestion-div');
 
-//event for submit button
-document.querySelector(".search-button").addEventListener("click", () => {
-  const item= document.querySelector(".product-search").value;
-  console.log(item);
+searchBox.addEventListener('input', async () => {
+    let value = searchBox.value.trim();
+    if (value.length === 0) {
+        suggestionBox.innerHTML = "";
+        return;
+    }
+
+    let response = await fetch(`http://localhost:8080/api/product?query=${value}`);
+    let products = await response.json();
+
+    suggestionBox.innerHTML = products
+        .map(p => `<div class="suggestion-item">${p.name}</div>`)
+        .join("");
+
+    document.querySelectorAll('.suggestion-item').forEach((item) => {
+        item.addEventListener('click', () => {
+            searchBox.value = item.textContent;
+            suggestionBox.innerHTML = "";
+        });
+    });
 });
-
-
-
-const url="https://localhost:8080/api/proudcts"
-
- async function fetchProducts() {
-    const response=await fetch(url)
-    .json()
-}
